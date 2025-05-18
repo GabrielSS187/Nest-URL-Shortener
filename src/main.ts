3a7ae3cfc53125e64f14194a109a9ad9015b7900
Gabriel Silva <gabriel127.gk@gmail.com>
2025-05-18 17:41:31 -0300

    chore(security): add Helmet for setting secure HTTP headers - install helmet - apply app.use(helmet()) in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.use(helmet());
   app.useLogger(app.get(Logger));
 
   app.setGlobalPrefix('api');
